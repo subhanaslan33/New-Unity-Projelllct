6cb839b963c1a538000ff010847fc4910772130a
subhanaslan33 <60701061+subhanaslan33@users.noreply.github.com>
2020-02-13 19:52:16 +0300

    Initial commit

---
+++ Assets/Hareket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Hareket : MonoBehaviour
+{
+
+    [SerializeField]
+    int Hiz = 10;
+
+
+    Rigidbody rb;
+    // Use this for initializaation
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); ///fiziksel özellikleri alınmış
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float yatay = Input.GetAxis("Horizontal");
+
+        float dikey = Input.GetAxis("Vertical");
+        Vector3 Hareket = new Vector3(yatay * Hiz, 0.0f, dikey * Hiz);
+        rb.AddForce(Hareket);//force güç ekle demek
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rb;
+        rb = GetComponent<Rigidbody>(); ///fiziksel özellikleri alınmış
+
+
+    }
+}
---
+++ Assets/Vedoo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vedoo : MonoBehaviour
+{
+
+    [SerializeField]
+
+    GameObject kure;
+    Vector3 aradakifark;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        aradakifark = transform.position - kure.transform.position;
+
+        //aradaki farkı buluyoruz
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = kure.transform.position + aradakifark;
+
+    }
+}
